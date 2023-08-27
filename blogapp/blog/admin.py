0ac0672ae3fbef5712d8fd3ead542852753d10c6
onur demir <onurdemir0309@gmail.com>
2023-08-27 18:15:08 +0300

    uploading the project to git hub

---
+++ blogapp/blog/admin.py
@@ -0,0 +1,19 @@
+from django.contrib import admin
+from .models import Blog, Category
+from django.utils.safestring import mark_safe
+# Register your models here.
+class Blogadmin(admin.ModelAdmin):
+    list_display = ("title","is_active","is_home","slug","selected_categories",)
+    list_editable = ("is_active","is_home",)
+    search_fields = ("title","description")
+    readonly_fields = ("slug",)
+    list_filter = ("categories","is_active","is_home")
+    def selected_categories(self, obj):
+        html = "<ul>"
+        for category in obj.categories.all():
+            html += "<li>" + category.name + "</li>"
+        html += "</ul>"
+        return mark_safe(html)
+    
+admin.site.register(Blog,Blogadmin)
+admin.site.register(Category)
